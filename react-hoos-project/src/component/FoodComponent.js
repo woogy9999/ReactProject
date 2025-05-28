f3d89858e7df149ebe333c339abb4ab5fa039268
woogy9999 <gusdnrdkshf@gmail.com>
2025-05-28 16:23:27 +0900

    reactHook(25.05.22)

---
+++ react-hoos-project/src/component/FoodComponent.js
@@ -0,0 +1,12 @@
+function FoodComponent() {
+
+    /*
+        Hooks
+        1. 변수
+            지역변수 : let, const
+            props : 속성값으로 받는 값 => 불변 (정적 상태값)
+            state : 컴포넌트가 가질 수 있는 동적 상태값 (이벤트시에 값을 변경) /
+     */
+}
+
+export default FoodComponent;
